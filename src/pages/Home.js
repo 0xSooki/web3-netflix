6b61e500698662a2222969fe2fe3e6572818bc2a
stbence <stb3nc3@gmail.com>
2022-05-04 17:15:50 +0200

    feat(Home.js): nav bar, wallet connect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import { Logo } from "../images/Netflix";
+import { ConnectButton, Icon, TabList, Tab } from "web3uikit";
 
 const Home = () => {
-
-return(
-  <>
-  <div className="container">
-     <Link to="/player" className="link">Player Page</Link>
-  </div>
-  </>
-)
-}
+  return (
+    <>
+      <div className="flex justify-center align-center">
+        <Logo />
+      </div>
+      <div className="connect">
+        <Icon fill="#ffffff" size={24} svg="bell" />
+        <ConnectButton />
+      </div>
+      <div className="topBanner">
+        <TabList defaultActiveKey={1} tabStyle="bar">
+          <Tab tabKey={1} tabName={"Movies"}></Tab>
+          <Tab tabKey={2} tabName={"Series"}></Tab>
+          <Tab tabKey={3} tabName={"MyList"}></Tab>
+        </TabList>
+      </div>
+    </>
+  );
+};
 
 export default Home;
