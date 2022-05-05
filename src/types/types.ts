fe869774debadec3f7d034b8d27bd698cc1861ce
stbence <stb3nc3@gmail.com>
2022-05-05 08:53:13 +0200

    add(*): movie type, movie list

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export interface Movie {
+  Movie: string,
+  Thumnbnail: string,
+  Scene: string,
+  Logo: string,
+  Name: string,
+  Description: string,
+  Genre: string,
+  Actors: string,
+  Year: string,
+  Duration: string,
+}
