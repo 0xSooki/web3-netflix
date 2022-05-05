0d34180a0a59a734de2a45bb181a8b470cb9b3e6
stbence <stb3nc3@gmail.com>
2022-05-05 09:27:05 +0200

    feat(Home.tsx): display movies, movie preview

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
 import Logo from "../images/Netflix";
-import { ConnectButton, Icon, TabList, Tab, Button } from "web3uikit";
+import { ConnectButton, Icon, TabList, Tab, Button, Modal } from "web3uikit";
 import { movies } from "../helpers/library";
 import React from "react";
+import { useState } from "react";
+import { Movie } from "../types/types";
 
 const Home = () => {
+  const [visible, setVisible] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie>();
+
   return (
     <>
       <div className="logo">
@@ -39,10 +44,78 @@ const Home = () => {
                 />
               </div>
             </div>
+            <div className="title">Movies</div>
+            <div className="thumbs">
+              <>
+                {movies &&
+                  movies.map((m) => {
+                    return (
+                      <img
+                        src={m.Thumnbnail}
+                        className="thumbnail"
+                        alt=""
+                        onClick={() => {
+                          setSelectedMovie(m);
+                          setVisible(true);
+                        }}
+                      />
+                    );
+                  })}
+              </>
+            </div>
           </Tab>
           <Tab tabKey={2} tabName={"Series"}></Tab>
           <Tab tabKey={3} tabName={"MyList"}></Tab>
         </TabList>
+        {selectedMovie && (
+          <div className="modal">
+            <Modal
+              onCloseButtonPressed={() => setVisible(false)}
+              isVisible={visible}
+              hasFooter={false}
+              width="1000px"
+            >
+              <div className="modalContent">
+                <img src={selectedMovie.Scene} className="modalImg" alt="" />
+                <img src={selectedMovie.Logo} className="modalLogo" alt="" />
+                <div className="modalPlayButton">
+                <Button
+                  id="test-button-primary"
+                  onClick={function noRefCheck() {}}
+                  text="Primary Button"
+                  theme="secondary"
+                  type="button"
+                />
+                <Button
+                  id="test-button-primary"
+                  onClick={function noRefCheck() {}}
+                  text="Primary Button"
+                  theme="translucent"
+                  type="button"
+                />
+              </div>
+              <div className="movieInfo">
+                <div className="description">
+                  <div className="details">
+                    <span>{selectedMovie.Year}</span>
+                    <span>{selectedMovie.Duration}</span>
+                  </div>
+                  {selectedMovie.Description}
+                </div>
+                <div className="detailedInfo">
+                  Genre: <span className="deets">
+                    {selectedMovie.Genre}
+                  </span>
+                  <br />
+                  Actors: <span className="deets">
+                    {selectedMovie.Actors}
+                  </span>
+                </div>
+              </div>
+              </div>
+            </Modal>
+          </div>
+        )}
       </div>
     </>
   );
